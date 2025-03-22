85f0fc7cebf6d2ee513670928c353d672f9b886d
小弟调调 <kennyiseeyou@gmail.com>
2025-03-22 14:42:32 +0800

    doc: Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SearchField",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
